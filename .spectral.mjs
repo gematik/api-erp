ff330d49d602439453be3d78765436ec29535c9b
florian.schoffke@gematik.de <florian.schoffke@gematik.de>
2023-06-06 09:49:18 +0200

    Added .spectral.mjs
    Committed by github.com on behalf of florian.schoffke@gematik.de

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwNDUxOA/spectral.js?branch_slug=feature%2Fapi-2-0&token=bd5cf945-8fdd-4521-8405-1a69e2b5e755";
+export default { extends: ruleset };
