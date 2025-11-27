31018c0e06921d7253241afa1e03a00b9a7c92e0
Hendre Janse van Rensburg <hendre.vanrensburg@gematik.de>
2025-11-27 13:52:03 +0000

    update the source code to include the copyright in the header

--- ReceiptSignatureVerificationPoC/src/main/java/de/gematik/test/erp/cms/CMSParser.java
+++ ReceiptSignatureVerificationPoC/src/main/java/de/gematik/test/erp/cms/CMSParser.java
@@ -1,3 +1,23 @@
+/*
+Copyright (Change Date see Readme), gematik GmbH
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*******
+
+For additional notes and disclaimer from gematik and in case of changes
+by gematik, find details in the "Readme" file.
+*/
 package de.gematik.test.erp.cms;
 
 import de.gematik.pki.tsl.TslInformationProvider;
